0a64183a981adcc5d8aab539b2d3d76577703f3b
Mariela-t <91850798+Mariela-t@users.noreply.github.com>
2022-02-12 12:10:56 -0800

    added an overview of the weather and form for search

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import Weather from "./Weather";
+import Overview from "./Overview";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="boxWrapper">
+      <div className="box">
+        <form className="mb-4">
+          <input
+            type="search"
+            placeholder="Enter city..."
+            className="form-control mb-3"
+            autoComplete="off"
+          />
+          <input type="submit" value="Search" className="button" />
+        </form>
+        <Overview />
+        <Weather />
+      </div>
     </div>
   );
 }
